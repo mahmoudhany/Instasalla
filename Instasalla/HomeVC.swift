4ce7b1bcf1165b33abce7ff62e3898ad7fcd2448
mahmoudhany <ma7moud.hany158@gmail.com>
2017-12-19 19:04:45 +0200

    x

--- Instasalla/HomeVC.swift
+++ Instasalla/HomeVC.swift
@@ -8,50 +8,51 @@
 import UIKit
 import IGListKit
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController, ListAdapterDataSource {
      
-     @IBOutlet weak var collectionView: IGListCollectionView!
+     lazy var adapter: ListAdapter = {
+          return ListAdapter(updater: ListAdapterUpdater(), viewController: self, workingRangeSize: 1)
+     }()
+
+     let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
      
- 
+     let data = [128, 256, 64]
+     
      override func viewDidLoad() {
           super.viewDidLoad()
- 
           
+          collectionView.backgroundColor = UIColor.lightGray
+          view.addSubview(collectionView)
+          adapter.collectionView = collectionView
+          adapter.dataSource = self
+     }
+     
+     override func viewDidLayoutSubviews() {
+          super.viewDidLayoutSubviews()
+          collectionView.frame = view.bounds
+     }
+     
+     // MARK: ListAdapterDataSource
+     
+     func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
+          return data as [ListDiffable]
      }
      
      
-     
-     
-}
-
-class CategorySection: IGListSectionController{
-     
-     
-
-
-}
-extension CategorySection: ListSectionType {
-      func numberOfItems() -> Int {
-          return 1
+     func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+          // note that each child section controller is designed to handle an Int (or no data)
+          let sectionController = ListStackedSectionController(sectionControllers: [
+               
+               ])
+          
+          sectionController.inset = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
+          return sectionController
      }
      
-      func sizeForItem(at index: Int) -> CGSize {
-          return CGSize(width: 375, height: 80)
-     }
+     func emptyView(for listAdapter: ListAdapter) -> UIView? { return nil }
      
-      func cellForItem(at index: Int) -> UICollectionViewCell {
-          let cell = IGListCollectionContext!.dequeueReusableCell(withNibName: .reuseIdentifier, bundle: Bundle.main, for: self, at: index)
-          
-          if let cell = cell as? Charactable, let character = self.character{
-               cell.setup(character: character)
-          }
-          
-          return cell
-}
-
-
-
-
+     
+     
 }
 
 
@@ -60,26 +61,3 @@ extension CategorySection: ListSectionType {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//          let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ProductsCell
-//
-//          cell.productImage.image = UIImage(named: "food.jpg")
-//          cell.productsCount.text = "123 Products"
-//          cell.productType.text = "Frozen"
-//
-//          return cell
-//     }
